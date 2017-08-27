a850cf1097c8604c1265255ef3881101c92ddbf3
jradcliffe5 <jack.radcliffe@postgrad.manchester.ac.uk>
2017-08-27 11:52:36 +0200

    added machine readable catalog

--- SAD_catalog.py
+++ SAD_catalog.py
@@ -15,14 +15,35 @@ import itertools
 from time import gmtime, strftime, localtime
 import pyfits
 import numpy as np
-
+### inputs ###
 AIPS.userno = 1002
 i=1
 auto_rms = True
 rms = 4.73189515179e-05
 edge = 100
 rms_box=250
-os.system('rm catalogue.txt detections.txt')
+
+def SAD_fit_remove(files):
+    if os.path.isfile('catalogue.csv') == False:
+        s = 'Catalog_name rms #      Peak    Dpeak     Flux    Dflux    RA---SIN   DEC--SIN   Dx      Dy       Maj     Min      PA    Dmaj    Dmin    Dpa #  MAJ-fit MIN-fit PA-fit    MAJ-dec MIN-dec PA-dec  R  MAJ-low MIN-low PA-low    MAJ-hi  MIN-hi  PA-hi    Xpix   Ypix   MAXresid\n'
+        s = ' '.join(s.split())+'\n'
+        s = s.replace(' ',',')
+        os.system('touch catalogue.csv')
+        text_file = open('catalogue.csv','a')
+        text_file.write(s)
+    for j in files:
+        with open(j) as f:
+            x = f.read().splitlines()
+        remove = ['#','Component']
+        for i in remove:
+            x = [y for y in x if not i in y]
+        x = [y for y in x if y != ' ']
+        x = [y.lstrip() for y in x]
+        x = ' '.join(x).replace('(',' ').replace(')',' ')
+        x = ' '.join(x.split())
+        text_file.write(x.replace(' ',',')+'\n')
+os.system('rm catalogue.csv detections.txt')
+
 detections = []
 for file in os.listdir('./'):
     if file.endswith('IM.fits'):
@@ -64,5 +85,31 @@ os.system('touch detections.txt')
 thefile = open('detections.txt', 'w')
 for item in detections:
   thefile.write("%s\n" % item)
-os.system('cat *fitout > catalogue.txt')
+
+catalog_list = []
+for file in os.listdir('./'):
+    if file.endswith('fitout'):
+        catalog_list = catalog_list + [file]
+SAD_fit_remove(catalog_list)
 os.system('rm *fitout')
+
+def substring(string_list):
+    out=[]
+    for s in string_list:
+        if not any([s in r for r in string_list if s != r]):
+            out.append(s)
+    return out
+
+def make_SAD_catalogue(file):
+    try:
+        f = open(file,'rw+')
+    except RunTimeError:
+        print 'No sources detected'
+    line = f.readlines()
+    thing = ['Peak','Component','MAJ-fit']
+    for i in thing:
+        line = [x for x in line if i not in x]
+    y = []
+    x=[]
+
+make_SAD_catalogue('catalogue.txt')
